ffc3c4946bc2c05f4b329d5fc959739d35753cce
Martin Gonzalez <mgonzalez738@gmail.com>
2020-08-19 21:57:38 -0300

    Subiendo www y node modules

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require("./middleware/errorHandler");
@@ -12,11 +13,12 @@ const logRiegosRoutes = require('./routes/logRiegosRoutes');
 const app = express();
 
 // Habilita todos los pedidos CORS
+
 app.use(cors());
 
 // Parsea el body de los pedidos como JSONs y completa req.body con las claves
+
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../frontend/www")));
 
 // Rutas
 
@@ -29,10 +31,26 @@ app.get('/api', function (req, res) { // Documentacion
   res.sendFile('/doc/apiDoc.html',  { root: __dirname });
 });
 
+// Sirve la aplicacion angular (Funciona el router)
+// https://stackoverflow.com/questions/49640365/mean-nodejs-server-for-angular-app-how-do-i-serve-angular-routes
 
-app.get('/', function (req, res) { // Frontend
-  res.sendFile('/index.html',  { root: __dirname + "/../frontend/wwww"});
+var staticRoot = path.join(__dirname, "../frontend/www/");
+
+app.use(function(req, res, next) {
+  //if the request is not html then move along
+  var accept = req.accepts('html');
+  if (accept !== 'html') {
+      return next();
+  }
+  // if the request has a '.' assume that it's for a file, move along
+  var ext = path.extname(req.path);
+  if (ext !== '') {
+      return next();
+  }
+  fs.createReadStream(staticRoot + 'index.html').pipe(res);
 });
+
+app.use(express.static(staticRoot));
 
 // Maneja errores
 
